ca776f2f9ca09dfd06ca0b96b931236fcf50503a
Michal Konopka <konopek68@gmail.com>
2023-10-23 20:51:32 +0200

    run test

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,11 @@ fn main() {
             Ok(mut stream) => {
                 let ok_response = "HTTP/1.1 200 OK\r\n\r\n";
                 stream.write_all(ok_response.as_bytes()).unwrap();
-                println!("send response");
+
+                // let response = "HTTP/1.1 200 OK\r\n\r\n";
+                // stream.write_all(response.as_bytes()).unwrap();
+
+                println!("accepted new connection");
             }
             Err(e) => {
                 println!("error: {}", e);
