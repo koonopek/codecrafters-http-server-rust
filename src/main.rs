5c1fd41c08470963a901205ceaebd4dfd731ab9c
Michal Konopka <konopek68@gmail.com>
2023-11-12 09:30:11 +0100

    feat: add reading file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::{
+    borrow::BorrowMut,
     collections::HashMap,
+    fs,
     io::{BufRead, BufReader, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
+    path::{Path, PathBuf},
 };
 
 use http_server_starter_rust::ThreadPool;
@@ -11,20 +14,27 @@ const OK_RESPONSE: &[u8; 19] = b"HTTP/1.1 200 OK\r\n\r\n";
 const NOT_FOUND_RESPONSE: &[u8; 26] = b"HTTP/1.1 404 Not Found\r\n\r\n";
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let default_arg = String::from(".");
+    let second_arg = args.get(2).unwrap_or(&default_arg);
+
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
     let thread_pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
+        let base_dir = second_arg.clone();
         let stream = stream.unwrap();
         thread_pool
-            .run(Box::new(|| handle_connection(stream)))
+            .run(Box::new(move || {
+                handle_connection(stream, base_dir.as_str())
+            }))
             .unwrap();
         println!("dispatched request")
     }
 }
 
-fn handle_connection(mut stream: std::net::TcpStream) {
+fn handle_connection(mut stream: std::net::TcpStream, base_dir: &str) {
     println!("accepted new connection");
 
     let request_buffer = BufReader::new(&stream);
@@ -34,7 +44,6 @@ fn handle_connection(mut stream: std::net::TcpStream) {
         .map(|line| line.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    // we are not collecting body yet
 
     let http_request = parse_request(&http_request_lines).unwrap();
 
@@ -45,12 +54,18 @@ fn handle_connection(mut stream: std::net::TcpStream) {
     {
         let user_agent = http_request.headers.get("User-Agent").unwrap().as_str();
         stream
-            .write_all(ok_response(user_agent).as_bytes())
+            .write_all(ok_text_response(user_agent).as_bytes())
             .unwrap();
     } else if http_request.path.starts_with("/echo") {
         stream
-            .write_all(ok_response(&&http_request.path[6..]).as_bytes())
+            .write_all(ok_text_response(&&http_request.path[6..]).as_bytes())
             .unwrap();
+    } else if http_request.path.starts_with("/files/") {
+        let file = http_request.path.split("/").last().unwrap_or("");
+        let mut path = PathBuf::new();
+        path.push(base_dir);
+        path.push(file);
+        serve_file(stream, &path)
     } else {
         stream.write_all(NOT_FOUND_RESPONSE).unwrap();
     }
@@ -103,7 +118,7 @@ fn parse_path(first_line: Option<&String>) -> Result<String, Result<HttpRequest,
     Ok(path)
 }
 
-fn ok_response(body: &str) -> Vec<u8> {
+fn ok_text_response(body: &str) -> Vec<u8> {
     let mut response = b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n".to_vec();
     let content_length = body.len();
 
@@ -113,4 +128,20 @@ fn ok_response(body: &str) -> Vec<u8> {
     response.extend_from_slice(body.as_bytes());
 
     response
+}
+
+fn serve_file(mut stream: TcpStream, file_path: &Path) {
+    match fs::read(file_path) {
+        Ok(mut file_content) => {
+            let mut response = OK_RESPONSE.to_vec();
+            let len = file_content.len();
+            response.extend(format!("Content-Type: application/octet-stream\r\n").as_bytes());
+            response.extend(format!("Content-Length: {len}\r\n\r\n").as_bytes());
+            response.append(&mut file_content);
+            stream.write_all(&response).unwrap();
+        }
+        Err(_) => {
+            stream.write_all(NOT_FOUND_RESPONSE);
+        }
+    }
 }
